a5950752850c2a01c5ba9b795290403dceed9820
lh <3510105404@qq.com>
2024-07-02 17:11:44 +0800

    almost completed

--- CommentArea.go
+++ CommentArea.go
@@ -6,11 +6,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// 统一response结构
+type Response struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data any    `json:"data"`
+}
+
+// 评论结构
 type Comment struct {
 	ID             int       `json:"id"`
 	UserName       string    `json:"name"`
@@ -18,32 +27,27 @@ type Comment struct {
 	CommentContent string    `json:"content"`
 }
 
+// 主函数，分配路由
 func main() {
 	r := http.NewServeMux()
 
-	r.HandleFunc("/comment/add", HandleAddComment)
-	r.HandleFunc("/comment/get", HandleGetComments)
-	r.HandleFunc("/comment/delete", HandleDeleteComment)
+	r.HandleFunc("/comment/add", ResponseHandler(HandleAddComment))
+	r.HandleFunc("/comment/get", ResponseHandler(HandleGetComment))
+	r.HandleFunc("/comment/delete", ResponseHandler(HandleDeleteComment))
 
 	http.ListenAndServe(":8080", r)
 }
 
-func HandleAddComment(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
+// 把三个函数封装成一个函数，方便调用
+type handler func(w http.ResponseWriter, r *http.Request) (data interface{}, err error)
 
-	if r.Method == "OPTIONS" {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
+// 添加评论,遇到错误直接返回
+func HandleAddComment(w http.ResponseWriter, r *http.Request) (data interface{}, err error) {
 	var NewComment Comment
-	err := json.NewDecoder(r.Body).Decode(&NewComment)
+	err = json.NewDecoder(r.Body).Decode(&NewComment)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		return nil, err
 	}
 	fmt.Println("Comment", NewComment)
 	defer r.Body.Close()
@@ -51,29 +55,129 @@ func HandleAddComment(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("mysql", "root:liuhan73@tcp(localhost:3306)/myfirstbase")
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 	defer db.Close()
 
 	err = InsertComment(db, NewComment.UserName, time.Now(), NewComment.CommentContent)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(NewComment)
+	return NewComment, nil
 }
 
-func HandleGetComments(w http.ResponseWriter, r *http.Request) {
+// 获取评论
+func HandleGetComment(w http.ResponseWriter, r *http.Request) (data interface{}, err error) {
+	db, err := sql.Open("mysql", "root:liuhan73@tcp(localhost:3306)/myfirstbase")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer db.Close()
+	queryvalue := r.URL.Query()
+	page, _ := strconv.Atoi(queryvalue.Get("page"))
+	size, _ := strconv.Atoi(queryvalue.Get("size"))
 
+	type pagedata struct {
+		Total    int       `json:"total"`
+		Comments []Comment `json:"comments"`
+	}
+	var comments []Comment
+	comments, err = GetComment(db, page, size)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil, err
+	}
+	Pagedata := pagedata{len(comments), comments}
+	return Pagedata, nil
 }
 
-func HandleDeleteComment(w http.ResponseWriter, r *http.Request) {
+// 删除评论
+func HandleDeleteComment(w http.ResponseWriter, r *http.Request) (data interface{}, err error) {
+	db, err := sql.Open("mysql", "root:liuhan73@tcp(localhost:3306)/myfirstbase")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer db.Close()
+	queryvalue := r.URL.Query()
+	id, _ := strconv.Atoi(queryvalue.Get("id"))
+	err = DeleteComment(db, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil, err
+	}
+	return nil, nil
+}
 
+// 对handler函数进行封装，统一返回格式
+func ResponseHandler(h handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		data, err := h(w, r)
+		//遇到错误返回错误的response
+		if err != nil {
+			resp := Response{Code: 1, Msg: err.Error(), Data: nil}
+			json.NewEncoder(w).Encode(resp)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		//否则返回成功的response
+		resp := Response{Code: 0, Msg: "success", Data: data}
+		json.NewEncoder(w).Encode(resp)
+		w.WriteHeader(http.StatusOK)
+	}
 }
 
 func InsertComment(db *sql.DB, UserName string, CommentTime time.Time, CommentContent string) error {
 	_, err := db.Exec("insert into comments(name,time,content) values(?,?,?)", UserName, CommentTime, CommentContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetComment(db *sql.DB, page int, size int) ([]Comment, error) {
+	var rows *sql.Rows
+	var err error
+	fmt.Println("page", page, "size", size)
+	if size == -1 {
+		rows, err = db.Query("select id,name,content from comments")
+	} else {
+		rows, err = db.Query("select id,name,content from comments limit ?,?", (page-1)*size, size)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var comments []Comment
+	for rows.Next() {
+		var c Comment
+		err = rows.Scan(&c.ID, &c.UserName, &c.CommentContent)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+	fmt.Println(comments)
+	return comments, nil
+}
+
+func DeleteComment(db *sql.DB, id int) error {
+	fmt.Println("delete id", id)
+	_, err := db.Exec("delete from comments where id=?", id)
 	if err != nil {
 		return err
 	}
--- CommentsArea.js
+++ CommentsArea.js
@@ -1,22 +1,24 @@
 var currentpage=1;
+getcomments(currentpage,-1);
 var comments=document.getElementsByClassName("comment");
 const PrevPage=document.getElementById("prev-page");
 const NextPage=document.getElementById("next-page");
-const deletebuttons=document.getElementsByClassName("delete-button");
+var deletebuttons=document.getElementsByClassName("delete-button");
+
+//默认显示第一页评论
+ShowComments(currentpage,comments);
+
 //给每个delete按钮添加事件
 for(let i=0;i<deletebuttons.length;i++){
     deletebuttons[i].addEventListener("click",function(){
         const confirmation = confirm('你确定要删除此评论吗？'); // 弹出确认对话框
         if(confirmation){
-            fetch("http://localhost:8080/comment/delete/",{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:JSON.stringify({
-            })
+            deletecomment(this.parentNode.id);
+            this.parentNode.remove();
+        }
     })
 }
+
 //上一页
 PrevPage.addEventListener("click",function(){
     if (currentpage>1){
@@ -24,6 +26,7 @@ PrevPage.addEventListener("click",function(){
     }
     ShowComments(currentpage,comments);
 })
+
 //下一页
 NextPage.addEventListener("click",function(){
     if(currentpage<comments.length/5){
@@ -31,6 +34,7 @@ NextPage.addEventListener("click",function(){
     }
     ShowComments(currentpage,comments);
 })
+
 //显示部分评论
 function ShowComments(currentpage,comments){
     for(let i=0;i<comments.length;i++){
@@ -45,9 +49,7 @@ function ShowComments(currentpage,comments){
         }
     }
 }
-
-ShowComments(currentpage,comments);//默认显示第一页评论
-
+//添加评论
 const submitbutton=document.getElementById("submit");
 submitbutton.addEventListener("click",function(event){
     event.preventDefault();
@@ -66,15 +68,64 @@ submitbutton.addEventListener("click",function(event){
         body:JSON.stringify(postdata)
     })
     .then(response => response.json())
-    .then(data => {
-        console.log("评论成功",data);
-        insertcomment(data);
+    .then(responsejson => {
+        console.log("评论成功",responsejson);
+        insertcomment(responsejson.data);
     })
     .catch(error => {
         console.log(error);
     })
 })
 
+//获取评论
+function getcomments(currentpage,pagesize){
+    const url=`http://localhost:8080/comment/get?page=${currentpage}&size=${pagesize}`;
+    fetch(url,{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+    .then(response => 
+        response.json()
+    )
+    .then(responsejson => {
+        console.log("获取评论成功",responsejson);
+        const existingcomments=document.getElementsByClassName("comment");
+        for(;existingcomments.length>0;){
+            existingcomments[0].remove();
+        }
+        for(let i=0;i<responsejson.data.total;i++){
+            insertcomment(responsejson.data.comments[i]);
+        }
+        comments=document.getElementsByClassName("comment");
+    })
+    .catch(error => {
+        throw error;
+    })
+}
+
+//删除评论
+function deletecomment(id){
+    const url=`http://localhost:8080/comment/delete?id=${id}`
+    fetch(url,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+    .then(response=>
+        response.json()
+    )
+    .then(responsejson=>{
+        console.log("删除评论成功",responsejson);
+    })
+    .catch(error=>{
+        throw error;
+    })
+}
+
+//在页面中插入新评论的元素
 function insertcomment(data){
     const showarea=document.getElementById("show-field");
     //创建新的评论元素
@@ -83,11 +134,20 @@ function insertcomment(data){
     var newcontent=document.createElement("p");
     var newdeletebutton=document.createElement("button");
     //给新元素设置属性
+    newcomment.id=data.id;
     newcomment.className="comment";
     newusername.className="show-UserName";
     newcontent.className="show-CommentContent";
     newdeletebutton.className="delete-button";
     newdeletebutton.type="button";
+    //给新按钮添加事件
+    newdeletebutton.addEventListener("click",function(){
+        const confirmation = confirm('你确定要删除此评论吗？'); // 弹出确认对话框
+        if(confirmation){
+            deletecomment(this.parentNode.id);
+            this.parentNode.remove();
+        }
+    })
     //给新元素添加文本
     newusername.textContent=data.name;
     newcontent.textContent=data.content;
